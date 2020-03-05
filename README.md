e04d2eef141b16a751216fb0be6a60914cff676f
Juan Pablo Cervantes Gonzalez <cer11glez@gmail.com>
2020-03-05 15:05:16 -0600

    add function

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Base de datos de Virus más famosos que han atacado a los humanos en los último
 1. Nombre 
 2. Url de su fotografía
 3. Descripción
-4. Donde surgio)
+4. Donde surgio
 
 
--- js/js.js
+++ js/js.js
@@ -1,9 +1,98 @@
-//Variables
+//Obtener datos de local storage
+
+ Obtener=function(dato){
+       let datoLS;
+    //comprobamos si hay algo en localStorage
+    if (localStorage.getItem('datos') === null) {
+        datoLS = [];
+
+    } else {
+        datoLS = JSON.parse(localStorage.getItem('datos'));
+    }
+
+    return datoLS;
+ }
+ Agregar=function(id){
+       
+     const nombre = document.getElementById('nombre').value;
+     const urlfoto = document.getElementById('urlfoto').value;
+     const descripcion = document.getElementById('descripcion').value;
+     const dns = document.getElementById('dns').value;
+
+var miObjeto = { 'nombre': nombre, 'urlfoto': urlfoto, 'descripcion': descripcion, 'dns': dns };
+
+AgregarLS(miObjeto,0);
+ }
+
+ AgregarLS=function(dato,id){
+  let datos;
+    //Toma el valor de localStorage o vacio dependiendo de si hay datos
+   // datos = Obtener();
+    //El curso seleccionado se agrega al arreglo
+   // datos.push(dato);
+    //    
+    localStorage.setItem('datos'+1, JSON.stringify(dato));
+
+ }
+//Pasar al siguiente registro     Siguiente=function()
+{
+if(registroactual==(productos.length-1))
+alert("Este es el ultimo dato");
+else
+{
+    registroactual++;
+    mostrarProducto(registroactual);
+}
+}
+
+//Pasar al anterior registro     
+Anterior=function()
+{
+if(registroactual==0)
+alert("Este es el primer dato");
+else
+{
+    registroactual--;
+    mostrarProducto(registroactual);
+}
+}
+
+//Pasar al primero registro     
+Primero=function()
+{
+if(productos.length==0)
+alert("Ya no hay mas datos");
+else
+{
+    registroactual=0;
+    mostrarProducto(registroactual);
+}
+}
+
+//Mostrar el registro indicado
+mostrarProducto=function(no)
+ {
+    document.getElementById("nombre").value=localStorage.key(0).nombre; 
+    document.getElementById("descripcion").value=localStorage.getItem('datos'+0).descripcion;
+    document.getElementById("dns").value=localStorage.getItem('datos'+0).dnds;
+
+    document.getElementById("urlfoto").value=localStorage.getItem('datos'+0).urlfoto;
+    document.getElementById("foto").src=localStorage.getItem('datos'+0).urlfoto;
+    document.getElementById("barra").innerHTML="<b>"+(registroactual+1)+" de "+productos.length + " productos" +"</b>" ;
+
+    if (localStorage.getItem('datos') === null) {
+    
+
+    } else {
+
+    }
 
 
-//Listeners
+ }
 
+ var productos=[];
+ var registroactual=0;
 
+ mostrarProducto(registroactual);
 
-
-//Funciones
+ </script>   
